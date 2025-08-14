c1869b0b160b0dc23ec21058de216aef21a2ea02
Rayan <ray1607529@gmail.com>
2025-08-14 19:22:03 +0530

    Add files via upload

---
+++ 20.Nat_Num.c
@@ -0,0 +1,16 @@
+//Print First 10 Natural Numbers
+
+#include <stdio.h>
+
+int main(){
+    int i=1;
+    
+    printf("The First 10 Natural Numbers Are:\n");
+    
+    while (i<=10)
+    {
+     printf("%d\n",i);
+     i++;
+    }
+    
+}
