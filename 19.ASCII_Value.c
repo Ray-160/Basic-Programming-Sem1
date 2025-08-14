eb7afd139606423c738908f387dc1efc104f6dc4
Rayan <ray1607529@gmail.com>
2025-08-14 18:33:55 +0530

    Add files via upload

---
+++ 19.ASCII_Value.c
@@ -0,0 +1,16 @@
+//Prints ASCII value of given character
+
+#include <stdio.h>
+
+int main(){
+    char ch;
+
+    printf("Enter Character: ");
+    scanf("%c",&ch);
+
+    printf("The ASCII Value of %c is %d", ch, ch);
+
+    return 0;
+}
+
+
