f278b5ac7208ac0e11e6fd172e27c2829e8e8115
Rayan <ray1607529@gmail.com>
2025-09-05 11:32:35 +0530

    Add files via upload

---
+++ 58-Max_Array.cpp
@@ -0,0 +1,32 @@
+//Finds Max Elements In An Aray
+
+#include <iostream>
+
+int main(){
+ 	int size;
+ 	int array[size];
+ 	int max=0;
+ 	
+ 	std::cout << "Enter Size Of Array: ";
+ 	std::cin >> size;
+ 	
+ 	for (int i= 1; i<=size; i++)
+ 	{
+ 	 std::cout << "Enter Element: ";
+ 	 std::cin >> array[i];
+ 	}
+ 	
+    for (int j= 0; j<size+1; j++)
+    {    
+      if (array[j]>max)
+      	{     
+      	 max= array[j];
+      	}
+    }
+    
+    std::cout << "The Largest Elements Is: " << max << std::endl;
+    
+    return 0;
+    
+} 
+    	
